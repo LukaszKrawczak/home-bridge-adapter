f029250b5e687906376177178cda6db8f742071b
Lukasz Krawczak <lukasz.krawczak@statsperform.global>
2022-12-30 11:10:19 +0100

    Init commit

---
+++ src/test/java/com/brus5/homebridge/adapter/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brus5.homebridge.adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
