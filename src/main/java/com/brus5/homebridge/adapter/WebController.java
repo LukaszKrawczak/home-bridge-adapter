3e01d61265f9048c3cc7b166bc18f18fa682f7d1
Lukasz Krawczak <lukasz.krawczak@statsperform.global>
2023-01-01 20:33:00 +0100

    Adding jsonassert dependency, fix unit tests for parametrized, refactoring code in controller

--- src/main/java/com/brus5/homebridge/adapter/WebController.java
+++ src/main/java/com/brus5/homebridge/adapter/WebController.java
@@ -7,15 +7,20 @@ import com.brus5.homebridge.adapter.model.access.token.AccessTokenResponseDto;
 import com.brus5.homebridge.adapter.model.device.DeviceResponseDto;
 import com.brus5.homebridge.adapter.request.RequestAccessTokenHandler;
 import com.brus5.homebridge.adapter.request.RequestTempSensorHandler;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
@@ -36,24 +41,26 @@ public class WebController {
         return ResponseEntity.ok(accessToken);
     }
 
-    @RequestMapping(value = "/temp-sensor/1", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String tempSensorSalon() throws IOException, InterruptedException {
-        final AccessTokenResponseDto accessTokenResult = objectMapper.readValue(accessToken().getBody(), AccessTokenResponseDto.class);
-        final DeviceResponseDto response = requestTempSensorHandler.getTempSensor(accessTokenResult.getAccessTokenResultDto().getAccessToken(), tuyaConfiguration.getSalonTempSensor());
+    @RequestMapping(value = "/temp-sensor/{roomName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String tempSensorSalon(@PathVariable String roomName) throws IOException, InterruptedException {
+        if (roomName.equals("salon")) {
+            return getTemperatureSensor(tuyaConfiguration.getSalonTempSensor());
+        }
+
+        if (roomName.equals("bedroom")) {
+            return getTemperatureSensor(tuyaConfiguration.getBedroomTempSensor());
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Room name not found");
+    }
+
+    private String getTemperatureSensor(String deviceId) throws IOException, InterruptedException {
+        AccessTokenResponseDto accessTokenResult = objectMapper.readValue(accessToken().getBody(), AccessTokenResponseDto.class);
+        DeviceResponseDto response = requestTempSensorHandler.getTempSensor(accessTokenResult.getAccessTokenResultDto().getAccessToken(), deviceId);
         TempSensorModel tempSensorModel = new TempSensorModel();
         tempSensorModel.setTemperature(stringHelper.convertStringNumberToTemperature(response.getDeviceResultDto().getStatusDtos().get(0).getValue()));
         tempSensorModel.setHumidity(response.getDeviceResultDto().getStatusDtos().get(1).getValue());
+        objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
         return objectMapper.writeValueAsString(tempSensorModel);
     }
-
-    @RequestMapping(value = "/temp-sensor/2", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String tempSensorBedroom() throws IOException, InterruptedException {
-        final AccessTokenResponseDto accessTokenResult = objectMapper.readValue(accessToken().getBody(), AccessTokenResponseDto.class);
-        final DeviceResponseDto response = requestTempSensorHandler.getTempSensor(accessTokenResult.getAccessTokenResultDto().getAccessToken(), tuyaConfiguration.getBedroomTempSensor());
-        TempSensorModel tempSensorModel = new TempSensorModel();
-        tempSensorModel.setTemperature(stringHelper.convertStringNumberToTemperature(response.getDeviceResultDto().getStatusDtos().get(0).getValue()));
-        tempSensorModel.setHumidity(response.getDeviceResultDto().getStatusDtos().get(1).getValue());
-        return objectMapper.writeValueAsString(tempSensorModel);
-    }
-
 }
--- src/test/java/com/brus5/homebridge/adapter/WebControllerTest.java
+++ src/test/java/com/brus5/homebridge/adapter/WebControllerTest.java
@@ -1,7 +1,14 @@
 package com.brus5.homebridge.adapter;
 
+import org.json.JSONException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.skyscreamer.jsonassert.Customization;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.comparator.CustomComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -37,17 +44,38 @@ class WebControllerTest {
     }
 
     @Test
-    void shouldReturnTrue_whenRequestForDeviceIsCorrect_givenTmpHmdSensorUri() {
+    void shouldThrowResponseStatusException_whenRequestCannotResolveUri_givenTmpHdmSensorUriForNothing() {
         // given
-        String testedUri = "http://localhost:" + port + "/temp-sensor/2";
+        String testedUri = "http://localhost:" + port + "/temp-sensor/SOMETHING_THAT_CANNOT_PARSE";
 
         // when
-        final ResponseEntity<String> responseEntity = this.restTemplate.getForEntity(testedUri,
-                String.class);
+        final ResponseEntity<String> responseEntity = this.restTemplate.getForEntity(testedUri, String.class);
 
         // then
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"salon", "bedroom"})
+    void shouldReturnCorrectTemperatureAndHumidityResponse_whenExecutingGetTempSensorEndpoint_givenCorrectUri(String source) throws JSONException {
+        // given
+        String testedUri = "http://localhost:" + port + "/temp-sensor/" + source;
+
+        // when
+        String expected = "{" + "\"temperature\": \"23.1\","
+                + "\"humidity\": \"40\""
+                + "}";
+        final ResponseEntity<String> responseEntity = this.restTemplate.getForEntity(testedUri, String.class);
+
+        // then
+        JSONAssert.assertEquals(expected, responseEntity.getBody(), new CustomComparator(JSONCompareMode.LENIENT,
+                ignoreFieldValue("temperature"),
+                ignoreFieldValue("humidity"))
+        );
         Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertThat(responseEntity.getBody()).contains("temperature");
-        assertThat(responseEntity.getBody()).contains("humidity");
+    }
+
+    private Customization ignoreFieldValue(String ignoredFieldValue) {
+        return new Customization(ignoredFieldValue, (o1, o2) -> true);
     }
 }
